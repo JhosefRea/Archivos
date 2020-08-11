57da232134ff6e310b6cc5001ccbfc9652f45bc2
JhosefJr17 <66775663+JhosefRea@users.noreply.github.com>
2020-08-11 16:40:17 -0500

    Add files via upload
    Búsqueda lineal de un arreglo empleando funciones y almacenando en un archivo (C++)

---
+++ Taller.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int busqueda(int a[], int n, int dato);
+int main(){
+    ofstream archivo;
+    int a[5] = {4,1,3,2,5};
+    int dato;
+    archivo.open("Lineal.txt", ios::out);
+    
+    if(archivo.fail()){
+        cout << "No se puedo abrir el archivo " << endl;
+    }
+
+    archivo << "El arreglo de elementos es: " << endl;
+
+    for(int i = 0; i < 5; i++){
+        archivo << a[i] << " ";
+    }
+
+    cout << "digite un número: ";
+    cin >> dato;
+    archivo << "\nEl elemento a buscar es: " << dato << endl;
+    
+    if(busqueda(a, 5, dato) == -1){
+        archivo << "El elemento no se ha encontrado" << endl;
+
+    }else{
+        archivo << "El Elemento " << dato << " se ha encontrado en la posición " << busqueda(a,5,dato) << endl; 
+    }
+
+
+    
+    return 0;
+}
+
+int busqueda(int a[], int n, int dato){
+    for(int i = 0; i <= n; i++){
+        if(a[i] == dato){
+            return i;       
+        }
+    }
+    return -1;
+}
